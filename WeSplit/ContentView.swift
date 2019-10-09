4bfebf443ac1f536696c5273a3a72822b6f0106d
Peter Barclay <peter@peter-barclay.com>
2019-10-09 08:47:37 +0100

    ForEach Text Row

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -5,12 +5,11 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var name = ""
-
     var body: some View {
         Form {
-            TextField("Enter your name", text: $name)
-            Text("Your name is \(name)")
+            ForEach(0 ..< 100) { number in
+                Text("Row \(number)")
+            }
         }
     }
 }
