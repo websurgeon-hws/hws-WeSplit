9c61d0bfb0ee0956988fded6138c5fc3c964f1cb
Peter Barclay <peter@peter-barclay.com>
2019-10-10 07:40:05 +0100

    Picker in a Form

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -6,21 +6,30 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeopleIndex = 0
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", text: $checkAmount)
-                    .keyboardType(.decimalPad)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", text: $checkAmount)
+                        .keyboardType(.decimalPad)
+                    
+                    Picker("Number of people", selection: $numberOfPeopleIndex) {
+                        ForEach(2 ..< 100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section {
+                    Text("$\(checkAmount)")
+                }
             }
-            
-            Section {
-                Text("$\(checkAmount)")
-            }
+            .navigationBarTitle("WeSplit")
         }
     }
 }
